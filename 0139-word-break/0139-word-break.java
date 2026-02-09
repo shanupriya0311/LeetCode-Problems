27673e5f41f97fcb21b604220f5584c3672c7be7
Shanupriya T <shanupriya0311@gmail.com>
2026-02-09 12:11:47 +0530

    Time: 1 ms (99.61%), Space: 43.4 MB (82.98%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,34 +1,29 @@
 class Solution {
-
-    Boolean[] memo;   // null = not visited
-
+    Boolean[] memo;
     public boolean wordBreak(String s, List<String> wordDict) {
-        memo = new Boolean[s.length()];
-        return backtrack(s, wordDict, 0);
+        memo=new Boolean[s.length()];
+        return backtrack(s,wordDict,0);
     }
-
-    public boolean backtrack(String s, List<String> wordDict, int start) {
-
-        if(start == s.length()) return true;
-
-        // already computed
-        if(memo[start] != null) return memo[start];
-
-        for(String word : wordDict){
-
-            int len = word.length();
-
-            if(start + len <= s.length() &&
-               s.substring(start, start + len).equals(word)){
-
-                if(backtrack(s, wordDict, start + len)){
-                    memo[start] = true;
-                    return true;
-                }
-            }
+    public boolean backtrack(String s,List<String>worddic,int start){
+        if(start==s.length()){
+            return true;
         }
-
-        memo[start] = false;
-        return false;
-    }
-}
+        if(memo[start]!=null){
+            return memo[start];
+        }
+        for(int i=0;i<worddic.size();i++){
+            if(s.charAt(start)==worddic.get(i).charAt(0)){
+                int len=worddic.get(i).length();
+                if(start+len<=s.length()){
+                    if(s.substring(start,start+len).equals(worddic.get(i))){
+                    if(backtrack(s,worddic,start+len)){
+                        memo[start]=true;
+                        return true;
+                    }
+                }
+            }}
+                }
+            memo[start]=false;
+            return false;
+        }
+};
