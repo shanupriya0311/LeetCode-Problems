f004fc9a67c92bdbf4d7eb96cca9755d5518daa2
Shanupriya T <shanupriya0311@gmail.com>
2026-03-08 09:13:02 +0530

    Time: 27 ms (43.67%), Space: 47.3 MB (59.18%) - LeetHub

---
+++ 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int minFlips(String s) {
+        int n = s.length();
+        String s2 = s + s;
+
+        int alt1 = 0; // mismatches with 0101...
+        int alt2 = 0; // mismatches with 1010...
+
+        int res = Integer.MAX_VALUE;
+
+        for (int i = 0; i < s2.length(); i++) {
+
+            char expected1 = (i % 2 == 0) ? '0' : '1';
+            char expected2 = (i % 2 == 0) ? '1' : '0';
+
+            if (s2.charAt(i) != expected1) alt1++;
+            if (s2.charAt(i) != expected2) alt2++;
+
+            if (i >= n) {
+                char prev1 = ((i - n) % 2 == 0) ? '0' : '1';
+                char prev2 = ((i - n) % 2 == 0) ? '1' : '0';
+
+                if (s2.charAt(i - n) != prev1) alt1--;
+                if (s2.charAt(i - n) != prev2) alt2--;
+            }
+
+            if (i >= n - 1) {
+                res = Math.min(res, Math.min(alt1, alt2));
+            }
+        }
+
+        return res;
+    }
+}
