18e242134f424d1386eaa7201b9408faa808c71f
Shanupriya T <shanupriya0311@gmail.com>
2026-03-25 20:39:18 +0530

    Time: 0 ms (100%), Space: 45.2 MB (20.97%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -1,32 +1,32 @@
-
 class Solution {
-    // Method to determine if the target is in rotated sorted array or not
     public boolean search(int[] nums, int target) {
-        // Initialize the left and right bound
-        int left = 0, right = nums.length - 1;
-
-        // Iterate over the array
-        while (left <= right) {
-            // Get the mid value
-            int mid = left + (right - left) / 2;
-            if (nums[mid] == target) {
-                return true;
+        int n=nums.length;
+        int low=0;
+        int  high=n-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target){
+               return true;
             }
-            if (nums[left] < nums[mid]) {
-                if (nums[left] <= target && target < nums[mid]) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
-                }
-            } else if (nums[left] > nums[mid]) {
-                if (nums[mid] < target && target <= nums[right]) {
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
-                }
-            } else {
-                left++;
+             if(nums[mid]==nums[low]){
+                low++;
+                continue;
             }
+            if(nums[low]<=nums[mid]){
+                if(nums[low]<=target && target<nums[mid]){
+                    high=mid-1;  
+                }
+                else{
+                    low=mid+1;
+                }
+            }
+            else {
+            if(nums[mid]<target && target<=nums[high]){
+                    low=mid+1;   
+               }
+               else{
+                high=mid-1;
+               }}
         }
         return false;
     }
