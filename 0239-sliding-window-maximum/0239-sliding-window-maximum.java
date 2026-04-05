b911ef2c05c8f207f3853c460f9ecebc8890043f
Shanupriya T <shanupriya0311@gmail.com>
2026-04-05 14:42:48 +0530

    Time: 75 ms (14.28%), Space: 158.3 MB (9.66%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[0] - a[0]);
+
+        int m = nums.length - k + 1;
+        int[] result = new int[m];
+
+        // first window
+        for (int i = 0; i < k; i++) {
+            pq.add(new int[]{nums[i], i});
+        }
+
+        int j = 0;
+        result[j++] = pq.peek()[0];
+
+        int l = k;
+        int s = 0;
+
+        while (l < nums.length) {
+            s++;
+
+            pq.add(new int[]{nums[l], l});
+            l++;
+
+            // remove out-of-window elements
+            while (pq.peek()[1] < s) {
+                pq.poll();   // ✅ use poll()
+            }
+
+            result[j++] = pq.peek()[0];
+        }
+
+        return result;
+    }
+}
