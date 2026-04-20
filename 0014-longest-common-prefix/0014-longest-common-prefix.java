53b8e7bef843caf9a50a4c3b976cf259f90c935b
Shanupriya T <shanupriya0311@gmail.com>
2026-04-20 10:41:47 +0530

    Time: 1 ms (63.17%), Space: 43 MB (84.66%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,30 +1,31 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        int n=strs.length;
-        String result="";
-        if(n==0){
-          return result;
-        }
-        int m=strs[0].length();
-        for(int i=0;i<m;i++){
-            char str=strs[0].charAt(i);
-            boolean res=true;
-            for(int j=0;j<n;j++){
-             if(i>=strs[j].length()|| strs[j].charAt(i)!=str){
-                res &=false;
-                break;
-             }
-             else if(strs[j].charAt(i)==str){
-                res &=true;
-             }
-            }
-            if(res){
-                result+=str;
-            }
-            else{
-                break;
+        String str = strs[0];
+        int n = str.length();
+
+        // ✅ find smallest string
+        for (int i = 1; i < strs.length; i++) {
+            if (n > strs[i].length()) {
+                str = strs[i];
+                n = strs[i].length();
             }
         }
-        return result;
+
+        int minprefix = n;
+
+        // ✅ compare with smallest string
+        for (int i = 0; i < strs.length; i++) {
+            int j = 0;
+
+            while (j < n && str.charAt(j) == strs[i].charAt(j)) {
+                j++;
+            }
+
+            if (j == 0) return "";
+
+            minprefix = Math.min(minprefix, j);
+        }
+
+        return str.substring(0, minprefix);
     }
 }
