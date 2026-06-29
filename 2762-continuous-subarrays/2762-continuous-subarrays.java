697ad34aefbf33e210f5a0e659b034918c51ad00
Shanupriya T <shanupriya0311@gmail.com>
2026-06-29 15:46:21 +0530

    Time: 124 ms (5.2%), Space: 124.8 MB (5.46%) - LeetHub

--- 2762-continuous-subarrays/2762-continuous-subarrays.java
+++ 2762-continuous-subarrays/2762-continuous-subarrays.java
@@ -1,22 +1,31 @@
 class Solution {
     public long continuousSubarrays(int[] nums) {
-        PriorityQueue<int[]> maxpq = new PriorityQueue<>((a, b) -> b[0] - a[0]);
-        PriorityQueue<int[]> minpq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
-       long count = 0;
-        int i = 0;
-        for (int j = 0; j < nums.length; j++) {
-            maxpq.offer(new int[]{nums[j], j});
-            minpq.offer(new int[]{nums[j], j});
-            while (maxpq.peek()[0] - minpq.peek()[0] > 2) {
-                i++;
-                while (maxpq.peek()[1] < i)
-                    maxpq.poll();
-
-                while (minpq.peek()[1] < i)
-                    minpq.poll();
+        PriorityQueue<int[]>maxpq=new PriorityQueue<>((a,b)->b[0]-a[0]);
+        PriorityQueue<int[]>minpq=new PriorityQueue<>((a,b)->a[0]-b[0]);
+        long count=0;
+        int i=0,j=0;
+        int n=nums.length;
+        while(j<n){
+            maxpq.offer(new int[]{nums[j],j});
+            minpq.offer(new int[]{nums[j],j});
+                int diff=maxpq.peek()[0]-minpq.peek()[0];
+                if(0<=diff &&  diff<=2){
+                    count+=j-i+1;
+                    j++;
+                }
+                else{
+                    i++;
+                    while(i>maxpq.peek()[1] || i>minpq.peek()[1]){
+                    if(i>maxpq.peek()[1]){
+                        maxpq.poll();
+                    }
+                    if(i>minpq.peek()[1]){
+                        minpq.poll();
+                    }
+                }
+                }
             }
-            count += j - i + 1;
-        }
+          
         return count;
     }
 }
